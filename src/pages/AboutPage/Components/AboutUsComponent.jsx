be0fb3282d5f4704ea4ed7aa5ec4bb9a8dc67144
Thenukad Dinu <thenukadinu@gmail.com>
2021-02-18 00:53:44 +0530

    reorganized file structure. remove unused files and created folder structure for the project.

---
+++ src/pages/AboutPage/Components/AboutUsComponent.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function AboutUsComponent() {
+    return (
+        <div>
+            About Us
+        </div>
+    )
+}
---
+++ src/pages/HomePage/HomePageComponent.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function HomePageComponent() {
+    return (
+        <div>
+            Hello This is home page
+        </div>
+    )
+}
